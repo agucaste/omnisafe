867625f4c755ea9bf69cc561b29081ff8be490d5
hopkins <agustincastellano17@gmail.com>
2024-05-17 16:25:58 -0400

    plotter fixes

--- examples/my_examples/test_plotter.py
+++ examples/my_examples/test_plotter.py
@@ -2,11 +2,14 @@ from omnisafe.utils.my_plotter import MyPlotter
 
 if __name__ == '__main__':
     import os
-    os.chdir('/Users/agu/PycharmProjects/omnisafe/examples/my_examples/runs/TRPOLagBinaryCritic-{SafetyPointCircle1-v0}')
+    import glob
+    os.chdir('./runs/TRPOLagBinaryCritic-{SafetyPointCircle1-v0}/')
+    print(os.getcwd())
     experiments = {
-        'safest': ['./seed-000-2024-05-16-14-53-20', './seed-001-2024-05-16-14-53-24', './seed-999-2024-05-16-14-53-28'],
-        'redudant': ['./seed-000-2024-05-16-14-53-20']
+        'safest': ['./' + file for file in glob.glob('051724mac/*')] + ['./' + file for file in glob.glob('*16-15-12-53')],
+        'first_safe': ['./' + file for file in glob.glob('*05-16-15-15-37')],
+        'safest & Bellman equality': ['./' + file for file in glob.glob('*05-16-15-16-3*')]
     }
     plotter = MyPlotter()
-    plotter.make_plots(experiments=experiments)
+    plotter.make_plots(experiments=experiments, smooth=5)
 
