bbba0813396b535fa72e006bcef996e82f8918d1
Agustin Castellano <agustincastellano17@gmail.com>
2024-10-16 10:32:32 -0400

    working PPOBC

---
+++ omnisafe/algorithms/my_algorithms/ppo_bc.py
@@ -0,0 +1,234 @@
+import torch
+from torch import nn
+from torch.nn.utils.clip_grad import clip_grad_norm_
+from torch.nn.functional import binary_cross_entropy
+
+from collections import deque
+from typing import Any
+
+from omnisafe.adapter.ppo_bc_policy_adapter import PPOBCPolicyAdapter
+from omnisafe.algorithms import registry
+from omnisafe.algorithms.on_policy.naive_lagrange import PPOLag
+from omnisafe.common import Lagrange
+from omnisafe.common.buffer.ppobc_buffer import PPOBCBuffer
+from omnisafe.models.actor_safety_critic import ActorCriticBinaryCritic
+from omnisafe.utils import distributed
+
+
+
+@registry.register
+class PPOBinaryCritic(PPOLag):
+
+    def _init(self) -> None:
+        self._lagrange: Lagrange = Lagrange(**self._cfgs.lagrange_cfgs)
+
+        self._buf: PPOBCBuffer = PPOBCBuffer(
+            obs_space=self._env.observation_space,
+            act_space=self._env.action_space,
+            size_on=self._steps_per_epoch,
+            size_off=self._cfgs.algo_cfgs.size_off_policy_buffer,
+            gamma=self._cfgs.algo_cfgs.gamma,
+            lam=self._cfgs.algo_cfgs.lam,
+            lam_c=self._cfgs.algo_cfgs.lam_c,
+            advantage_estimator=self._cfgs.algo_cfgs.adv_estimation_method,
+            batch_size=self._cfgs.algo_cfgs.batch_size,
+            penalty_coefficient=self._cfgs.algo_cfgs.penalty_coef,
+            standardized_adv_r=self._cfgs.algo_cfgs.standardized_rew_adv,
+            standardized_adv_c=self._cfgs.algo_cfgs.standardized_cost_adv,
+            device=self._device,
+        )
+
+    def _init_model(self) -> None:
+        self._actor_critic: ActorCriticBinaryCritic = ActorCriticBinaryCritic(
+            obs_space=self._env.observation_space,
+            act_space=self._env.action_space,
+            model_cfgs=self._cfgs.model_cfgs,
+            epochs=self._cfgs.train_cfgs.epochs,
+        ).to(self._device)
+
+        if distributed.world_size() > 1:
+            distributed.sync_params(self._actor_critic)
+
+        if self._cfgs.model_cfgs.exploration_noise_anneal:
+            self._actor_critic.set_annealing(
+                epochs=[0, self._cfgs.train_cfgs.epochs],
+                std=self._cfgs.model_cfgs.std_range,
+            )
+
+    def _init_env(self) -> None:
+        self._env: PPOBCPolicyAdapter = PPOBCPolicyAdapter(
+            self._env_id,
+            self._cfgs.train_cfgs.vector_env_nums,
+            self._seed,
+            self._cfgs,
+        )
+        assert self._cfgs.algo_cfgs.steps_per_epoch % (
+            distributed.world_size() * self._cfgs.train_cfgs.vector_env_nums
+        ) == 0, 'The number of steps per epoch is not divisible by the number of environments.'
+        self._steps_per_epoch: int = (
+            self._cfgs.algo_cfgs.steps_per_epoch
+            // distributed.world_size()
+            // self._cfgs.train_cfgs.vector_env_nums
+        )
+        self._update_count: int = 0
+
+    def _init_log(self) -> None:
+        super()._init_log()
+
+        self._logger.register_key('Loss/binary_critic_axiomatic')
+        self._logger.register_key('Loss/Loss_binary_critic')
+        self._logger.register_key('Value/binary_critic')
+
+        "05/28/24: Registries for binary classifier"
+        self._logger.register_key('Classifier/Accuracy')
+        self._logger.register_key('Classifier/Power')
+        self._logger.register_key('Classifier/Miss_rate')
+        # self._logger.register_key('Classifier/per_step_epochs')
+
+        # TODO: Move this to another place! here it's ugly.
+        self._actor_critic.initialize_binary_critic(env=self._env, cfgs=self._cfgs, logger=self._logger)
+
+        self._sampled_positions = deque(maxlen=self._cfgs.algo_cfgs.batch_size * 16)
+        what_to_save: dict[str, Any] = {'pi': self._actor_critic.actor,
+                                        'binary_critic': self._actor_critic.binary_critic,
+                                        'reward_critic': self._actor_critic.reward_critic,
+                                        'cost_critic': self._actor_critic.cost_critic,
+                                        'pos': self._sampled_positions}
+        if self._cfgs.algo_cfgs.obs_normalize:
+            obs_normalizer = self._env.save()['obs_normalizer']
+            what_to_save['obs_normalizer'] = obs_normalizer
+
+        self._logger.setup_torch_saver(what_to_save)
+        self._logger.torch_save()
+
+    def _update(self):
+        super()._update()
+        # Update the binary critic. ->
+        # Should be same number batch updates as for the on-policy counterpart.
+        for _ in range(self._cfgs.algo_cfgs.update_iters * self._cfgs.algo_cfgs.steps_per_epoch
+                       // self._cfgs.algo_cfgs.batch_size):
+            data = self._buf.sample_batch()
+            self._update_count += 1
+            obs, act, reward, cost, done, next_obs, pos = (
+                data['obs'],
+                data['act'],
+                data['reward'],
+                data['cost'],
+                data['done'],
+                data['next_obs'],
+                data['pos']
+            )
+            self._sampled_positions.extend(list(pos))
+            self._update_binary_critic(obs, act, next_obs, cost, reward)
+
+        # if self._update_count % self._cfgs.algo_cfgs.policy_delay == 0:
+        self._actor_critic.polyak_update(self._cfgs.algo_cfgs.polyak_binary)
+
+    def _update_binary_critic(self, obs: torch.Tensor, act: torch.Tensor,
+                              next_obs: torch.Tensor, cost: torch.Tensor, reward: torch.Tensor) -> None:
+        r"""Update value network under a double for loop.
+
+        The loss function is ``MSE loss``, which is defined in ``torch.nn.MSELoss``.
+        Specifically, the loss function is defined as:
+
+        .. math::
+
+            L = \frac{1}{N} \sum_{i=1}^N (\hat{V} - V)^2
+
+        where :math:`\hat{V}` is the predicted cost and :math:`V` is the target cost.
+
+        #. Compute the loss function.
+        #. Add the ``critic norm`` to the loss function if ``use_critic_norm`` is ``True``.
+        #. Clip the gradient if ``use_max_grad_norm`` is ``True``.
+        #. Update the network by loss function.
+
+        Args:
+            obs (torch.Tensor): The ``observation`` sampled from buffer.
+        """
+
+        self._actor_critic.binary_critic_optimizer.zero_grad()
+        values = self._actor_critic.binary_critic.assess_safety(obs, act)
+
+        with torch.no_grad():
+            if self._cfgs.algo_cfgs.bc_training == 'off-policy':
+                next_a, *_ = self._actor_critic.pick_safe_action(next_obs, criterion='safest', mode='off_policy')
+            elif self._cfgs.algo_cfgs.bc_training == 'on-policy':
+                next_a = self._actor_critic.predict(next_obs, deterministic=False)
+            else:
+                raise (ValueError, f'barrier training mode should be off-policy or on-policy, '
+                                   f'not {self._cfgs.algo_cfgs.bc_training}')
+
+            if self._cfgs.algo_cfgs.bc_training_labels == 'soft':
+                labels = self._actor_critic.target_binary_critic.assess_safety(next_obs, next_a)
+            elif self._cfgs.algo_cfgs.bc_training_labels == 'hard':
+                labels = self._actor_critic.target_binary_critic.get_safety_label(next_obs, next_a)
+            else:
+                raise (ValueError, "binary critic's labelling should be either 'soft' or 'hard', not"
+                                   f"{self._actor_critic.algo_cfgs.bc_training_labels}")
+        labels = torch.maximum(labels, cost).clamp_max(1)
+
+        # 07/05/24
+        # Regress each binary critic towards the consensus label.
+        FBCE = FilteredBCELoss(operator=self._cfgs.model_cfgs.operator)
+        loss = sum(
+            FBCE(pred, labels) for pred in self._actor_critic.binary_critic.assess_safety(obs, act, average=False)
+        )
+
+        if self._cfgs.algo_cfgs.use_critic_norm:
+            for param in self._actor_critic.binary_critic.parameters():
+                loss += param.pow(2).sum() * self._cfgs.algo_cfgs.critic_norm_coef
+
+        # print(f'Binary critic loss is {loss:.4f}')
+        if torch.isnan(loss):
+            print(f'Loss is NaN')
+            for i, v in enumerate(zip(values, labels)):
+                print(f'ix = {i}\nlhs: {v[0]}\trhs: {v[1]}\n')
+
+        loss.backward()
+
+        if self._cfgs.algo_cfgs.use_max_grad_norm:
+            clip_grad_norm_(
+                self._actor_critic.binary_critic.parameters(),
+                self._cfgs.algo_cfgs.max_grad_norm,
+            )
+        self._actor_critic.binary_critic_optimizer.step()
+
+        self._logger.store({'Loss/Loss_binary_critic': loss.mean().item(),
+                            'Value/binary_critic': values.mean().item(),
+                            },
+                           )
+
+        # Get classifier metrics?
+        metrics = self._actor_critic.binary_critic.classifier_metrics(values, labels)
+        self._logger.store(
+            # {'Classifier/per_step_epochs': epoch,
+            {
+                'Classifier/Accuracy': metrics['accuracy'].item(),
+                'Classifier/Power': metrics['power'].item(),
+                'Classifier/Miss_rate': metrics['miss_rate'].item()
+            },
+        )
+
+class FilteredBCELoss(nn.Module):
+    """
+    A filtered version of the BCELoss. Given predictions p_i and labels y_i,
+    Filters out the transitions that satisfy p_i >= 1/2 and y_i <= 1/2
+
+    """
+
+    def __init__(self, operator: str, threshold=0.5):
+        super().__init__()
+        if operator not in ['inequality', 'equality']:
+            raise (ValueError, "'operator' for binary critic should be 'inequality' or 'equality,"
+                               f"not {operator}")
+        self.operator = operator
+        self.threshold = threshold
+
+    def forward(self, predictions, targets):
+        if self.operator == 'inequality':
+            # 'mask' is the transitions that are being considered.
+            mask = ~torch.logical_and(predictions >= self.threshold, targets <= self.threshold)
+            loss = binary_cross_entropy(predictions[mask], targets[mask])
+        elif self.operator == 'equality':
+            loss = binary_cross_entropy(predictions, targets)
+        return loss
---
+++ omnisafe/common/buffer/ppobc_buffer.py
@@ -0,0 +1,217 @@
+# Copyright 2023 OmniSafe Team. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Implementation of OnPolicyBuffer."""
+
+from __future__ import annotations
+
+import torch
+
+from omnisafe.common.buffer import OnPolicyBuffer, OffPolicyBuffer
+from omnisafe.common.buffer.base import BaseBuffer
+from omnisafe.typing import DEVICE_CPU, AdvantageEstimator, OmnisafeSpace
+from omnisafe.utils import distributed
+from omnisafe.utils.math import discount_cumsum
+
+
+class PPOBCBuffer(object):  # pylint: disable=too-many-instance-attributes
+    """A buffer for storing trajectories experienced by an agent interacting with the environment.
+
+    Besides, The buffer also provides the functionality of calculating the advantages of
+    state-action pairs, ranging from ``GAE``, ``GAE-RTG`` , ``V-trace`` to ``Plain`` method.
+
+    .. warning::
+        The buffer only supports Box spaces.
+
+    Compared to the base buffer, the on-policy buffer stores extra data:
+
+    +----------------+---------+---------------+----------------------------------------+
+    | Name           | Shape   | Dtype         | Shape                                  |
+    +================+=========+===============+========================================+
+    | discounted_ret | (size,) | torch.float32 | The discounted sum of return.          |
+    +----------------+---------+---------------+----------------------------------------+
+    | value_r        | (size,) | torch.float32 | The value estimated by reward critic.  |
+    +----------------+---------+---------------+----------------------------------------+
+    | value_c        | (size,) | torch.float32 | The value estimated by cost critic.    |
+    +----------------+---------+---------------+----------------------------------------+
+    | adv_r          | (size,) | torch.float32 | The advantage of the reward.           |
+    +----------------+---------+---------------+----------------------------------------+
+    | adv_c          | (size,) | torch.float32 | The advantage of the cost.             |
+    +----------------+---------+---------------+----------------------------------------+
+    | target_value_r | (size,) | torch.float32 | The target value of the reward critic. |
+    +----------------+---------+---------------+----------------------------------------+
+    | target_value_c | (size,) | torch.float32 | The target value of the cost critic.   |
+    +----------------+---------+---------------+----------------------------------------+
+    | logp           | (size,) | torch.float32 | The log probability of the action.     |
+    +----------------+---------+---------------+----------------------------------------+
+
+    Args:
+        obs_space (OmnisafeSpace): The observation space.
+        act_space (OmnisafeSpace): The action space.
+        size_on (int): The size of the buffer.
+        gamma (float): The discount factor.
+        lam (float): The lambda factor for calculating the advantages.
+        lam_c (float): The lambda factor for calculating the advantages of the critic.
+        advantage_estimator (AdvatageEstimator): The advantage estimator.
+        penalty_coefficient (float, optional): The penalty coefficient. Defaults to 0.
+        standardized_adv_r (bool, optional): Whether to standardize the advantages of the actor.
+            Defaults to False.
+        standardized_adv_c (bool, optional): Whether to standardize the advantages of the critic.
+            Defaults to False.
+        device (torch.device, optional): The device to store the data. Defaults to
+            ``torch.device('cpu')``.
+
+    Attributes:
+        ptr (int): The pointer of the buffer.
+        path_start (int): The start index of the current path.
+        max_size (int): The maximum size of the buffer.
+        data (dict): The data stored in the buffer.
+        obs_space (OmnisafeSpace): The observation space.
+        act_space (OmnisafeSpace): The action space.
+        device (torch.device): The device to store the data.
+    """
+
+    def __init__(  # pylint: disable=too-many-arguments
+        self,
+        obs_space: OmnisafeSpace,
+        act_space: OmnisafeSpace,
+        size_on: int,
+        size_off: int,
+        gamma: float,
+        lam: float,
+        lam_c: float,
+        advantage_estimator: AdvantageEstimator,
+        batch_size: int,
+        penalty_coefficient: float = 0,
+        standardized_adv_r: bool = False,
+        standardized_adv_c: bool = False,
+        device: torch.device = DEVICE_CPU,
+    ) -> None:
+        """Initialize an instance of :class:`OnPolicyBuffer`."""
+        # super().__init__(obs_space, act_space, size, device)
+
+        self._on_policy_buffer: OnPolicyBuffer = OnPolicyBuffer(
+            obs_space, act_space, size_on,
+            gamma, lam, lam_c,
+            advantage_estimator, penalty_coefficient, standardized_adv_r, standardized_adv_c,
+            device,
+        )
+        # Add binary critic's key
+        self._on_policy_buffer.data['value_b'] = torch.zeros_like(self._on_policy_buffer.data['value_r'])
+
+        self._off_policy_buffer: OffPolicyBuffer = OffPolicyBuffer(
+            obs_space, act_space, size_off, batch_size, device,
+        )
+        self._off_policy_buffer.data['pos'] = torch.zeros((size_off, 1, 2), dtype=torch.float32, device=device)
+        self._device = device
+
+
+    def store(self, **data: torch.Tensor) -> None:
+        """Stores data in each buffer"""
+        # print(f'data to be stored\n{data}')
+        on_data = {k: data[k] for k in set(self._on_policy_buffer.data.keys()) & set(data.keys())}
+        off_data = {k: data[k] for k in set(self._off_policy_buffer.data.keys()) & set(data.keys())}
+
+        self._on_policy_buffer.store(**on_data)
+        self._off_policy_buffer.store(**off_data)
+
+    def finish_path(
+        self,
+        last_value_r: torch.Tensor | None = None,
+        last_value_c: torch.Tensor | None = None,
+        last_value_b: torch.Tensor | None = None,
+        idx: int = 0,
+    ) -> None:
+        """Finish the current path and calculate the advantages of state-action pairs.
+
+        On-policy algorithms need to calculate the advantages of state-action pairs
+        after the path is finished. This function calculates the advantages of
+        state-action pairs and stores them in the buffer, following the steps:
+
+        .. hint::
+            #. Calculate the discounted return.
+            #. Calculate the advantages of the reward.
+            #. Calculate the advantages of the cost.
+
+        Args:
+            last_value_r (torch.Tensor, optional): The value of the last state of the current path.
+                Defaults to torch.zeros(1).
+            last_value_c (torch.Tensor, optional): The value of the last state of the current path.
+                Defaults to torch.zeros(1).
+        """
+        if last_value_r is None:
+            last_value_r = torch.zeros(1, device=self._device)
+        if last_value_c is None:
+            last_value_c = torch.zeros(1, device=self._device)
+        if last_value_b is None:
+            last_value_b = torch.zeros(1, device=self._device)
+
+        path_slice = slice(self._on_policy_buffer.path_start_idx, self._on_policy_buffer.ptr)
+
+        last_value_r = last_value_r.to(self._device)
+        last_value_c = last_value_c.to(self._device)
+        last_value_b = last_value_b.to(self._device)
+
+        rewards = torch.cat([self._on_policy_buffer.data['reward'][path_slice], last_value_r])
+        values_r = torch.cat([self._on_policy_buffer.data['value_r'][path_slice], last_value_r])
+        costs = torch.cat([self._on_policy_buffer.data['cost'][path_slice], last_value_c])
+        values_c = torch.cat([self._on_policy_buffer.data['value_c'][path_slice], last_value_c])
+
+        values_b = torch.cat([self._on_policy_buffer.data['value_b'][path_slice], last_value_b])
+
+        discounted_ret = discount_cumsum(rewards, self._on_policy_buffer._gamma)[:-1]
+        self._on_policy_buffer.data['discounted_ret'][path_slice] = discounted_ret
+        rewards -= self._on_policy_buffer._penalty_coefficient * costs
+
+        adv_r, target_value_r = self._on_policy_buffer._calculate_adv_and_value_targets(
+            values_r,
+            rewards,
+            lam=self._on_policy_buffer._lam,
+        )
+        """
+        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        Key difference is in the following line!
+        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        """
+        adv_c, target_value_c = self._on_policy_buffer._calculate_adv_and_value_targets(
+            values_c * values_b,  # Key difference is here!!!
+            costs,
+            lam=self._on_policy_buffer._lam_c,
+        )
+
+        self._on_policy_buffer.data['adv_r'][path_slice] = adv_r
+        self._on_policy_buffer.data['target_value_r'][path_slice] = target_value_r
+        self._on_policy_buffer.data['adv_c'][path_slice] = adv_c
+        self._on_policy_buffer.data['target_value_c'][path_slice] = target_value_c
+
+        self._on_policy_buffer.path_start_idx = self._on_policy_buffer.ptr
+
+    def get(self) -> dict[str, torch.Tensor]:
+        """Get the data in the buffer.
+
+        .. hint::
+            We provide a trick to standardize the advantages of state-action pairs. We calculate the
+            mean and standard deviation of the advantages of state-action pairs and then standardize
+            the advantages of state-action pairs. You can turn on this trick by setting the
+            ``standardized_adv_r`` to ``True``. The same trick is applied to the advantages of the
+            cost.
+
+        Returns:
+            The data stored and calculated in the buffer.
+        """
+        return self._on_policy_buffer.get()
+
+    def sample_batch(self) -> dict[str, torch.Tensor]:
+        return self._off_policy_buffer.sample_batch()
+
